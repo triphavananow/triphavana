e774e134b3122bd67b34dc23bcd8c67770fca5e4
Aneli Valdes Acosta <avaldes@nisum.com>
2018-12-12 14:11:28 -0300

    Added handlerbars

--- webpack.config.js
+++ webpack.config.js
@@ -12,12 +12,25 @@ module.exports = {
     mode: 'development',
     resolve: { extensions: [".js", ".ts"] },
     watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.hbs$/,
+                loader: 'handlebars-loader',
+                options: {
+                    knownHelpersOnly: false,
+                    helperDirs: [path.join(__dirname, 'hbs-helpers')],
+                },
+            }
+        ]
+    },
     plugins: [
         new HtmlWebpackPlugin({
             hash: true,
             title: 'My Awesome application',
             myPageHeader: 'Hello World',
-            template: './src/index.html',
+            template: './src/index.hbs',
+            data: require('./data/index.json'),
             chunks: ['app'],
             filename: 'index.html' //will be in dist folder
         }),
@@ -25,7 +38,8 @@ module.exports = {
             hash: true,
             title: 'Services',
             myPageHeader: 'Hello Services',
-            template: './src/services.html',
+            template: './src/services.hbs',
+            data: require('./data/services.json'),
             chunks: ['services'],
             filename: 'services.html' //will be in dist folder
         })
