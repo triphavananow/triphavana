d3f17942e1983e9bbc35afdaa05463c03b1fa194
Aneli Valdes Acosta <avaldes@nisum.com>
2018-12-12 16:41:48 -0300

    webpackDevServer

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,9 @@ module.exports = {
         path: path.resolve(__dirname, './dist'),
         filename: '[name].bundle.js'
     },
+    devServer: {
+        contentBase: './dist'
+    },
     mode: 'development',
     resolve: { extensions: [".js", ".ts"] },
     watch: true,
